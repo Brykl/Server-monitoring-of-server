be3fc51799991c868ad7891c019d3391ffb25e18
Brykl <rokirok430@gmail.com>
2025-02-11 11:52:04 +0500

    set up creating a data on server and set up nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import { Request, Response, Next } from "restify";
+const getServerInfo = require('./services/informationOfServer/serverInfo')
+
 const restify = require('restify')
 
 const server = restify.createServer();
 
 server.get("/", (req: Request, res: Response, next: Next) => {
-  res.send({ message: "Hello, Restify with TypeScript!" });
+
+  res.send({ message: "Hello, Restify with TypeScript!", test: 'test' });
+  console.log('test')
+  console.log(getServerInfo())
+  console.log(getServerInfo())
   next();
 });
 
-server.listen(8090, () => {
-  console.log("Server is running on http://localhost:8090");
+server.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
 });
