506eaae3b044d3ede1d560cbbc283795adec6820
Brykl <rokirok430@gmail.com>
2025-02-11 14:36:41 +0500

    configured import data from server, and set ip allowed origin via cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,36 @@
 import { Request, Response, Next } from "restify";
+const corsMiddleware = require('restify-cors-middleware2')
+
+const cors = corsMiddleware({
+  origins: ["http://localhost:5173"],
+  allowHeaders: ["X-Requested-With"],
+});
+
 const getServerInfo = require('./services/informationOfServer/serverInfo')
 
 const restify = require('restify')
 
 const server = restify.createServer();
 
+server.use(
+  function crosOrigin(req: Request, res: Response, next: Next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+)
+
 server.get("/", (req: Request, res: Response, next: Next) => {
 
   res.send({ message: "Hello, Restify with TypeScript!", test: 'test' });
-  console.log('test')
-  console.log(getServerInfo())
-  console.log(getServerInfo())
   next();
 });
+
+server.get("/server/info", (req: Request, res: Response, next: Next) => {
+  res.send({data: getServerInfo()})
+  // res.send({message: 'Hello'})
+})
+
 
 server.listen(8080, () => {
   console.log("Server is running on http://localhost:8080");
